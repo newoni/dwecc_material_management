ec002c36e4c5591ecdd01b69748f5989ae37348c
newoni <newoni_@naver.com>
2021-08-31 12:36:31 +0900

    first commit 4 dwecc material management application <21.08.31>

---
+++ app/src/main/java/com/example/dwecc_material_management/BomCheck.java
@@ -0,0 +1,13 @@
+package com.example.dwecc_material_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BomCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bom_check);
+    }
+}
---
+++ app/src/main/java/com/example/dwecc_material_management/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dwecc_material_management;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button_bomCheck = findViewById(R.id.bomCheck);
+        Button button_insertMaterial2DB = findViewById(R.id.insertMaterial2DB);
+        Button button_deleteMaterial2DB = findViewById(R.id.deleteMaterial2DB);
+        Button button_readMaterial = findViewById(R.id.bomCheck);
+
+        button_bomCheck.setOnClickListener(new View.OnClickListener() { // 클릭리스터 생성
+            @Override // 부모 메소드 재정의
+            public void onClick(View v) { // 클릭 이벤트 처리
+                Intent intent = new Intent(MainActivity.this,
+                        BomCheck.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
